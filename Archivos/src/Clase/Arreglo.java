ad8fa339284e63f720503e1655aa7e637045aeae
Forero <92945796+ProgramasW@users.noreply.github.com>
2021-11-08 07:30:53 -0500

    Actualizacion

--- Archivos/src/Clase/Arreglo.java
+++ Archivos/src/Clase/Arreglo.java
@@ -5,145 +5,34 @@
  */
 package Clase;
 
-import java.util.ArrayList;
+import java.io.*;
+import java.nio.Buffer;
+import java.util.Vector;
 import javax.swing.JOptionPane;
-import Clase.Estudiante;
-import static archivos.Edicion.*;
-import static archivos.Lista.TablaLista;
-import archivos.Nuevo;
-import static archivos.Nuevo.*;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.PrintWriter;
-import java.util.StringTokenizer;
-import javax.swing.DefaultListModel;
-import javax.swing.table.DefaultTableModel;
 
-/**
- *
- * @author Hermes
- */
 public class Arreglo {
 
-    public static ArrayList<Estudiante> lista = new ArrayList();
-    Estudiante e = new Estudiante();
-    DefaultTableModel tabla = new DefaultTableModel();
-    DefaultListModel listaaa = new DefaultListModel();
-
-    public void Exito() {
-        try {
-            JOptionPane.showMessageDialog(null, "SE INGRESO CORRECTAMENTE");
-
-        } catch (Exception e) {
-            JOptionPane.showMessageDialog(null, "ERROR AL GUARDAR DATO");
-        }
-
+    Vector vector = new Vector();
+    
+    public void guardar(String alumno){
+        vector.addElement(alumno);
     }
-
-    public void Guardar() {
-        e.setCodigo(Integer.parseInt(txtCodigo.getText()));
-        e.setNombre(txtNombres.getText());
-        e.setApellidos(txtApellidos.getText());
-        e.setTelefono(txtTelefono.getText());
-        e.setSemestre(Integer.parseInt(txtSemestre.getText()));
-        lista.add(e);
-    }
-
-    public void guardarDatos(Estudiante estudiante) {
+    
+    public void guardarArchivo(Estudiante estudiante){
         try {
-            FileWriter fw = new FileWriter("Estudiantes.dat", true);
+            FileWriter fw = new FileWriter("Alumno.txt", true);
             BufferedWriter bw = new BufferedWriter(fw);
             PrintWriter pw = new PrintWriter(bw);
-            pw.print("" + e.getCodigo());
-            pw.print("," + e.getNombre());
-            pw.print("," + e.getApellidos());
-            pw.print("," + e.getTelefono());
-            pw.print("" + e.getSemestre());
-            pw.print("\n");
-            pw.close();
-
+            pw.print(estudiante.getCodigo());
+            pw.print(","+estudiante.getNombre());
+            pw.print(","+estudiante.getApellidos());
+            pw.print(","+estudiante.getTelefono());
+            pw.print(","+estudiante.getSemestre());
+            pw.close();         
+            
         } catch (Exception e) {
-            JOptionPane.showMessageDialog(null, "LLORELO :(");
-        }
-
-    }
-
-    public void limpiezaCasillas() {
-        txtCodigo.setText("");
-        txtNombres.setText("");
-        txtApellidos.setText("");
-        txtTelefono.setText("");
-        txtSemestre.setText("");
-
-    }
-
-    public void llenado()  {
-
-//        tabla.addColumn("CÓDIGO");
-//        tabla.addColumn("NOMBRES");
-//        tabla.addColumn("APELLIDOS");
-//        tabla.addColumn("TÉLEFONO");
-//        tabla.addColumn("SMTR");
-//        TablaLista.setModel(tabla);
-//        lista.add(e);
-
-DefaultTableModel dt = new DefaultTableModel(){
-            @Override
-            public boolean isCellEditable(int row, int column){
-                return false;
-            }
-        };
-        
-        dt.addColumn("Codigo");
-        dt.addColumn("Nombre");
-        dt.addColumn("APELLIDOS");
-        dt.addColumn("TELEFONO");
-        dt.addColumn("SEMESTRE");
-        
-        //FileReader fr = new FileReader("Estudiantes.dat");
-        //TablaLista.setDefaultRenderer("Estudiantes.dat");
-        Object fila[] = new Object[dt.getColumnCount()];
-        for(int i = 0; i < lista.size(); i++){
-            e = lista.get(i);
-            fila[0] = e.getCodigo();
-            fila[1] = e.getNombre();
-            fila[2] = e.getApellidos();
-            fila[3] = e.getTelefono();
-            fila[3] = e.getSemestre();
-            dt.addRow(fila);
-        }
-        TablaLista.setModel(dt);
-        TablaLista.setRowHeight(60);
-    
-
-    }
-
-
-    public void guardarDatos3() {
-        FileWriter fw;
-        PrintWriter pw;
-        try {
-            fw = new FileWriter("Estudiantes.dat");
-            pw = new PrintWriter(fw);
-
-            for (int i = 0; i < lista.size(); i++) {
-                e = lista.get(i);
-                pw.println(String.valueOf(e.getCodigo() + ", " + e.getNombre() + ", " + e.getApellidos() + ", " + e.getTelefono()+", "+e.getSemestre()));
-            }
-            pw.close();
-
-        } catch (Exception ex) {
-            JOptionPane.showMessageDialog(null, "ERROR AL CARGAR DATOS");
-
+            JOptionPane.showMessageDialog(null, e);
         }
     }
-    
-    
-    
-    
     
 }
