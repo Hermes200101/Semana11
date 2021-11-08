d0f2def372b87e666c2b54c202f79ada4ecc5470
HERMES CASTRO <hermes2001.hdcr89@gmail.com>
2021-11-07 20:49:36 -0500

    nuevaAct

--- Archivos/src/Clase/Arreglo.java
+++ Archivos/src/Clase/Arreglo.java
@@ -81,7 +81,7 @@ public class Arreglo {
 
     }
 
-    public void llenado() throws FileNotFoundException {
+    public void llenado()  {
 
 //        tabla.addColumn("CÓDIGO");
 //        tabla.addColumn("NOMBRES");
@@ -142,30 +142,8 @@ DefaultTableModel dt = new DefaultTableModel(){
         }
     }
     
-    public void cargar_txt(){
-        File ruta = new File("Estudiantes.dat");
-        try{
-            
-            FileReader fi = new FileReader(ruta);
-            BufferedReader bu = new BufferedReader(fi);
-            
-            
-            String linea = null;
-            while((linea = bu.readLine())!=null){
-                StringTokenizer st = new StringTokenizer(linea, ",");
-                e = new Producto();
-                e.setCodigo(Integer.parseInt(st.nextToken()));
-                e.setNombre(st.nextToken());
-                e.setPrecio(Double.parseDouble(st.nextToken()));
-                e.setDescripcion(st.nextToken());
-                lista.add(e);
-            }
-            bu.close();
-        }catch(Exception ex){
-            mensaje("Error al cargar archivo: "+ex.getMessage());
-            System.out.println(ex.getMessage());
-        }
-    }
+    
+    
     
     
 }
